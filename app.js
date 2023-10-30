07668582abb04d9611d6721b8ecd91614bea5043
kongukjae <51992661+kongukjae@users.noreply.github.com>
2023-10-30 21:40:16 +0900

    send() 메서드를 사용해 index.html을 보내보았지만, 문자열을 응답하는 증상을 확인했다. 파싱을 정의하는 다른 메서드가 있는 것을 '외워야' 한다.

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send(__dirname + '/public/index.html')
 })
 
 app.listen(port, () => {
