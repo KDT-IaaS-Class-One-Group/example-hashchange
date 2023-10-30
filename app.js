26a15735cbb7b5cdeafcc7827d846ab337f46b7f
kongukjae <51992661+kongukjae@users.noreply.github.com>
2023-10-30 22:02:40 +0900

    static page 작동 확인 완료

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const fs = require('fs');
+const path = require('path');
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send(__dirname + '/public/index.html')
-})
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.listen(port, () => {
-  console.log(`http://localhost:${port}`)
+  console.log(`http://localhost:${port}`);
 })
